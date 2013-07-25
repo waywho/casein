4ae5104d5207843f42568be810436a59beb84937
Russell Quinn <mail@russellquinn.com>
2013-07-24 18:37:28 -0700

    Rename test class

--- test/casein_test.rb
+++ test/casein_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class Casein3Test < ActiveSupport::TestCase
+class CaseinTest < ActiveSupport::TestCase
   # Replace this with your real tests.
   test "the truth" do
     assert true
