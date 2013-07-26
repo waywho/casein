bf036f12068e0d2d67e2e92b9df8f5f126493ae5
Russell Quinn <mail@russellquinn.com>
2013-07-26 15:47:09 -0700

    Remove unnecessary flash messages about logging in and out

--- app/controllers/casein/user_sessions_controller.rb
+++ app/controllers/casein/user_sessions_controller.rb
@@ -15,7 +15,6 @@ module Casein
     def create
       @user_session = Casein::UserSession.new params[:casein_user_session]
       if @user_session.save
-        flash[:notice] = "Login successful"
         redirect_back_or_default :controller => :casein, :action => :index
       else
         render :action => :new
@@ -24,7 +23,6 @@ module Casein
   
     def destroy
       current_user_session.destroy
-      flash[:notice] = "Logout successful"
       redirect_back_or_default new_casein_user_session_url
     end
 
