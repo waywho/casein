cf0eeb49e498abde6a3f6b1c88729e517a16daf6
Russell Quinn <russell@mcsweeneys.net>
2013-08-11 13:17:53 +0100

    config_helper tidy up

--- lib/generators/casein/install/templates/app/helpers/casein/config_helper.rb
+++ lib/generators/casein/install/templates/app/helpers/casein/config_helper.rb
@@ -1,43 +1,42 @@
 module Casein
   module ConfigHelper
     
-    # Text string containing the name of the website or client
-    # Used in text and titles throughout Casein
+    # Name of website or client — used throughout Casein.
     def casein_config_website_name
       'Casein'
     end
 
-    # Path to the logo used for the sign in screen and top banner - it should be a transparent PNG (around 140 x 30px)
+    # Filename of logo image. Ideally, it should be a transparent PNG around 140x30px
     def casein_config_logo
       'casein/casein.png'
     end
 
     # The server hostname where Casein will run
     def casein_config_hostname
-      if ENV['RAILS_ENV'] == 'production'
+      if Rails.env.production?
         'http://www.caseincms.com'
       else
         'http://0.0.0.0:3000'
       end
     end
 
-    # The sender email address used for notifications
+    # The sender address used for email notifications
     def casein_config_email_from_address
       'donotreply@caseincms.com'
     end
   
-    # The page that the user is shown when they sign in or click the logo
-    # do not point this at casein/index!
+    # The initial page the user is shown after they sign in or click the logo. Probably this should be set to the first tab.
+    # Do not point this at casein/index!
     def casein_config_dashboard_url
       url_for :controller => :casein, :action => :blank
     end
   
-    # A list of stylesheet files to include in the page head section
+    # A list of stylesheets to include. Do not remove the core casein/casein, but you can change the load order, if required.
     def casein_config_stylesheet_includes
       %w[casein/casein casein/custom]
     end
   
-    # A list of JavaScript files to include in the page head section
+    # A list of JavaScript files to include. Do not remove the core casein/casein, but you can change the load order, if required.
     def casein_config_javascript_includes
       %w[casein/casein casein/custom]
     end
