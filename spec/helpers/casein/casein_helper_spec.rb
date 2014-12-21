3316fc2594f9c8ce37fe3b57daef68f07097fb4f
Mr Rogers <jon@bunnymatic.com>
2014-12-20 18:53:44 -0800

    update readme with new field helpers and update spec names

--- spec/helpers/casein/casein_helper_spec.rb
+++ spec/helpers/casein/casein_helper_spec.rb
@@ -40,7 +40,7 @@ describe Casein::CaseinHelper, type: :helper do
     let(:builder) { ActionView::Helpers::FormBuilder.new :test_model, obj, helper, opts }
 
     describe "#casein_color_field" do
-      it "returns the correct version info" do
+      it "builds an html5 color field" do
         field = helper.casein_color_field(builder, obj, 'my_color')
         expect(field).to have_css('.form-group .well .casein-color input#test_model_my_color')
         expect(field).to have_css('.form-group label.control-label', "My Color")
@@ -48,7 +48,7 @@ describe Casein::CaseinHelper, type: :helper do
     end
 
     describe "#casein_telephone_field" do
-      it "returns the correct version info" do
+      it "builds an html5 telephone field" do
         field = helper.casein_telephone_field(builder, obj, 'my_telephone')
         expect(field).to have_css('.form-group .well .casein-telephone input#test_model_my_telephone')
         expect(field).to have_css('.form-group label.control-label', "My Telephone")
@@ -56,7 +56,7 @@ describe Casein::CaseinHelper, type: :helper do
     end
 
     describe "#casein_search_field" do
-      it "returns the correct version info" do
+      it "builds an html5 search field" do
         field = helper.casein_search_field(builder, obj, 'my_search')
         expect(field).to have_css('.form-group .well .casein-search input#test_model_my_search')
         expect(field).to have_css('.form-group label.control-label', "My Search")
@@ -64,7 +64,7 @@ describe Casein::CaseinHelper, type: :helper do
     end
 
     describe "#casein_url_field" do
-      it "returns the correct version info" do
+      it "builds an html5 url field" do
         field = helper.casein_url_field(builder, obj, 'my_url')
         expect(field).to have_css('.form-group .well .casein-url input#test_model_my_url')
         expect(field).to have_css('.form-group label.control-label', "My Url")
@@ -72,7 +72,7 @@ describe Casein::CaseinHelper, type: :helper do
     end
 
     describe "#casein_email_field" do
-      it "returns the correct version info" do
+      it "builds an html5 email field" do
         field = helper.casein_email_field(builder, obj, 'my_email')
         expect(field).to have_css('.form-group .well .casein-email input#test_model_my_email')
         expect(field).to have_css('.form-group label.control-label', "My Email")
@@ -80,7 +80,7 @@ describe Casein::CaseinHelper, type: :helper do
     end
 
     describe "#casein_date_field" do
-      it "returns the correct version info" do
+      it "builds an html5 date field" do
         field = helper.casein_date_field(builder, obj, 'my_date')
         expect(field).to have_css('.form-group .well .casein-date input#test_model_my_date')
         expect(field).to have_css('.form-group label.control-label', "My Date")
@@ -88,7 +88,7 @@ describe Casein::CaseinHelper, type: :helper do
     end
 
     describe "#casein_datetime_field" do
-      it "returns the correct version info" do
+      it "builds an html5 datetime field" do
         field = helper.casein_datetime_field(builder, obj, 'my_datetime')
         expect(field).to have_css('.form-group .well .casein-datetime input#test_model_my_datetime')
         expect(field).to have_css('.form-group label.control-label', "My Datetime")
@@ -96,7 +96,7 @@ describe Casein::CaseinHelper, type: :helper do
     end
 
     describe "#casein_datetime_local_field" do
-      it "returns the correct version info" do
+      it "builds an html5 datetime_local field" do
         field = helper.casein_datetime_local_field(builder, obj, 'my_datetime_local')
         expect(field).to have_css('.form-group .well .casein-datetime-local input#test_model_my_datetime_local')
         expect(field).to have_css('.form-group label.control-label', "My Datetime_Local")
@@ -104,7 +104,7 @@ describe Casein::CaseinHelper, type: :helper do
     end
 
     describe "#casein_month_field" do
-      it "returns the correct version info" do
+      it "builds an html5 month field" do
         field = helper.casein_month_field(builder, obj, 'my_month')
         expect(field).to have_css('.form-group .well .casein-month input#test_model_my_month')
         expect(field).to have_css('.form-group label.control-label', "My Month")
@@ -112,7 +112,7 @@ describe Casein::CaseinHelper, type: :helper do
     end
 
     describe "#casein_week_field" do
-      it "returns the correct version info" do
+      it "builds an html5 week field" do
         field = helper.casein_week_field(builder, obj, 'my_week')
         expect(field).to have_css('.form-group .well .casein-week input#test_model_my_week')
         expect(field).to have_css('.form-group label.control-label', "My Week")
@@ -120,7 +120,7 @@ describe Casein::CaseinHelper, type: :helper do
     end
 
     describe "#casein_time_field" do
-      it "returns the correct version info" do
+      it "builds an html5 time field" do
         field = helper.casein_time_field(builder, obj, 'my_time')
         expect(field).to have_css('.form-group .well .casein-time input#test_model_my_time')
         expect(field).to have_css('.form-group label.control-label', "My Time")
@@ -128,7 +128,7 @@ describe Casein::CaseinHelper, type: :helper do
     end
 
     describe "#casein_number_field" do
-      it "returns the correct version info" do
+      it "builds an html5 number field" do
         field = helper.casein_number_field(builder, obj, 'my_number', 1.0..20.0, 0.5)
         expect(field).to have_css('.form-group .well .casein-number input#test_model_my_number[step="0.5"]')
         expect(field).to have_css('.form-group label.control-label', "My Number")
@@ -136,7 +136,7 @@ describe Casein::CaseinHelper, type: :helper do
     end
 
     describe "#casein_range_field" do
-      it "returns the correct version info" do
+      it "builds an html5 range field" do
         field = helper.casein_range_field(builder, obj, 'my_range', 1.0..10.0)
         expect(field).to have_css('.form-group .well .casein-range input#test_model_my_range[max="10.0"]')
         expect(field).to have_css('.form-group label.control-label', "My Range")
