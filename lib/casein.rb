dcccece485732793688d5613696c2cc2c8d0d62b
Russell Quinn <russell@falsevacuum.com>
2015-01-05 20:16:26 -0800

    Fixed bug where version could not be found in built gem

--- lib/casein.rb
+++ lib/casein.rb
@@ -1,5 +1,6 @@
 if defined?(Rails) && Rails::VERSION::MAJOR == 4
-	require 'casein/engine' 
+	require 'casein/engine'
+	require 'casein/version'
 	require 'will_paginate'
 	require 'authlogic'
 else
