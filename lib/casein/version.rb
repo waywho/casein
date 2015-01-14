b470b1ae3e4344e549435b2733d0eb45a7a7c8d6
Russell Quinn <russell@falsevacuum.com>
2015-01-13 16:51:14 -0800

    Bumped version to 5.1.1.2 and removed legacy version files

--- lib/casein/version.rb
+++ lib/casein/version.rb
@@ -1,4 +1,4 @@
 module Casein
-  VERSION_HASH = { major: 5, minor: 1, patch: 1, build: 1 }
+  VERSION_HASH = { major: 5, minor: 1, patch: 1, build: 2 }
   VERSION = VERSION_HASH.values.join(".")
 end
