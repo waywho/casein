b53dc79506714024f4d6ed783c7858aee84fb28f
Russell Quinn <russell@falsevacuum.com>
2015-02-17 09:00:57 -0800

    Bump version number and date

--- lib/casein/version.rb
+++ lib/casein/version.rb
@@ -1,4 +1,4 @@
 module Casein
-  VERSION_HASH = { major: 5, minor: 1, patch: 1, build: 3 }
+  VERSION_HASH = { major: 5, minor: 1, patch: 2, build: 0 }
   VERSION = VERSION_HASH.values.join(".")
 end
